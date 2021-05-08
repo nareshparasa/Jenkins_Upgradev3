5eb59f06fad05113126bfa8d7d8d88a92691900e
Naresh, Parasa <ramnareshgoudp@gmail.com>
2021-05-08 13:56:43 +0530

    Change in Test Code

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -33,7 +33,7 @@ public class TestGreeter {
 
     @Test
   public void greetShouldIncludeGreetingMessage() {
-    String someone = "World, Thanks you making me Happy";
+    String someone = "World, This is Ram Naresh Parasa";
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
